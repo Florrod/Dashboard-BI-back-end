9a094ffe643d2b61f1797ff2bed36a198b290e62
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-07-16 20:00:07 +0000

    Se actualizan los modelos de la BBDD

--- src/models.py
+++ src/models.py
@@ -22,8 +22,9 @@ class Enterprise(db.Model):
     #     self.email = email
     #     self.is_active = is_active
 
-    # def __repr__(self):
-    #     return '<Enterprise %r>' % self.name
+    def __repr__(self):
+        return '<Enterprise %r>' % self.name
+
     def save(self):
         db.session.add(self)
         db.session.commit
@@ -49,14 +50,14 @@ class Brand(db.Model):
     logo= db.Column(db.String(120), nullable=True)
     enterprise_to_id = db.Column(db.Integer, db.ForeignKey('enterprise.id'), nullable=False)
     relation_integration = db.relationship('Integration', backref='brand', lazy=True)
-    relation_mi_data = db.relationship('Mydata', backref='brand', lazy=True)
+    relation_order = db.relationship('Order', backref='brand', lazy=True)
 
     # def __init__(self, name, logo):
     #     self.name = name
     #     self.logo = logo
 
-    # def __ref__(self):
-    #     return f'<Brand {self.name}>'
+    def __repr__(self):
+        return '<Brand %r>' % self.name
 
     def serialize(self):
         return {
@@ -64,7 +65,7 @@ class Brand(db.Model):
             "name": self.name,
             "logo": self.logo,
             "relation_integration": list(map(lambda x: x.serialize(), self.relation_integration)),
-            "relation_mi_data": list(map(lambda x: x.serialize(), self.relation_mi_data)),
+            "relation_order": list(map(lambda x: x.serialize(), self.relation_order)),
         }
     # def save(self):
     #     db.session.add(self)
@@ -75,17 +76,18 @@ class Integration(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     API_key= db.Column(db.String(120), nullable=True)
     # deleted = db.Column(db.Boolean(), default=False) #¿Esto está bien? hay que incluirlo en serialize y cómo
-    platform_id = db.relationship('Platform', backref='integration', lazy=True)
     brand_to_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+    platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'), nullable=False)
+    relation_order = db.relationship('Order', backref='integration', lazy=True)
     
-    # def __ref__(self):
-    #     return f'<Integration {self.id}>'
+    def __repr__(self):
+    return '<Integration %r>' % self.API_key
 
     def serialize(self):
         return {
             "id": self.id,
             "API_key": self.API_key,
-            "platform_id": list(map(lambda x: x.serialize(), self.platform_id))
+            "relation_order": list(map(lambda x: x.serialize(), self.relation_order)),
             # "deleted": self.deleted,
             # "relation_data": list(map(lambda x: x.serialize(), self.relation_data))
             # if not self.user.deleted else None
@@ -94,29 +96,94 @@ class Integration(db.Model):
 class Platform(db.Model):
     id= db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    relation_integration = db.Column(db.Integer, db.ForeignKey('integration.id'), nullable=False)
+    relation_client_platform = db.relationship('Client_Platform', backref='platform', lazy=True)
+    relation_integration = db.relationship('Integration', backref='platform', lazy=True)
 
-    # def __ref__(self):
-    #     return f'<Platform {self.name}>'
+    def __repr__(self):
+    return '<Platform %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
+            "relation_client_platform": list(map(lambda x: x.serialize(), self.relation_client_platform)),
+            "relation_integration": list(map(lambda x: x.serialize(), self.relation_integration)),
             # ¿hay que meter las relaciones?
-        }  
+        }
 
-class Mydata(db.Model):
+class Clients(db.Model):
     id= db.Column(db.Integer, primary_key=True)
-    detail = db.Column(db.String(250))
-    brand_to_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
-    integration_to_id = db.Column(db.Integer, db.ForeignKey('integration.id'), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    relation_order = db.relationship('Order', backref='clients', lazy=True)
+    relation_client_platform = db.relationship('Client_Platform', backref='clients', lazy=True)
 
-    def __ref__(self):
-        return f'<Mydata {self.id}>'
+    #preguntar lo del campo calculado de quantity orders
+
+    def __repr__(self):
+    return '<Clients %r>' % self.email
+
     def serialize(self):
         return {
             "id": self.id,
-            "detail": self.detail,
+            "email": self.email,
+            "relation_order": list(map(lambda x: x.serialize(), self.relation_order)),
+            "relation_client_platform": list(map(lambda x: x.serialize(), self.relation_client_platform)),
+            # ¿hay que meter las relaciones?
+        }  
+
+class Client_Platform(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.Integer) #preguntar si es int o string al ser estrellitas
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'), nullable=False)
+
+
+    def __repr__(self):
+    return '<Client_Platform %r>' % self.
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "review": self.review,
+            # ¿hay que meter las relaciones?
+        }
+
+class Order(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(250))
+    total_price = db.Column(db.Float)
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    integration_id = db.Column(db.Integer, db.ForeignKey('integration.id'), nullable=False)
+    relation_lineItem = db.relationship('LineItem', backref='order', lazy=True)
+
+    def __ref__(self):
+        return f'<Order {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "total_price": self.total_price,
+            "relation_lineItem": self.relation_lineItem
+            # ¿hay que meter las relaciones?
+        }
+
+class LineItem(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(250))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+
+    def __ref__(self):
+        return f'<LineItem {self.id}>'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_name": self.product_name,
+            "quantity": self.quantity,
+            "price": self.price
             # ¿hay que meter las relaciones?
         }
