374f3cf359793b3de2de01165f234dbc05dffd48
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-07-17 12:43:45 +0000

    Se cambia estilo del logIn

--- src/login_form.py
+++ src/login_form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Email
 
 class MyForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), Email(message='Please introduce a valid email')])
     password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
